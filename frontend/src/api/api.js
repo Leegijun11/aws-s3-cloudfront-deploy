113b919babdcbf6c1efd26f94af99eb01e522756
Lee gi jun <dlrlwms11@gmail.com>
2026-06-09 18:40:40 +0900

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, 
   timeout: 5000,
   
-  withCredentials: false, 
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
